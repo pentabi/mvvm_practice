69cce881d10d1ab129dc6614f8b1cd34922eb6da
pentabi <tabitosatoh@gmail.com>
2024-06-25 08:35:22 +0900

    色変えれるようになった

--- lib/view/widgets/todo_list_item.dart
+++ lib/view/widgets/todo_list_item.dart
@@ -2,24 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:mvvm_practice/model/todo_model.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem(
-      {super.key,
-      required this.todo,
-      required this.change,
-      required this.delete,
-      this.color = const Color.fromARGB(255, 255, 255, 255)});
+  const TodoListItem({
+    super.key,
+    required this.todo,
+    required this.change,
+    required this.delete,
+  });
 
   final TodoModel todo;
   final Function(bool?)? change;
   final Function() delete;
-  final Color color;
 
   @override
   Widget build(BuildContext context) {
+    // Determine the color based on todo.isChecked
+    final itemColor = todo.isChecked
+        ? const Color.fromARGB(255, 196, 246, 255)
+        : const Color.fromARGB(255, 255, 255, 255);
+
     return ListTile(
       title: Text(todo.taskName),
       subtitle: Text(todo.createdAt.toString()),
-      tileColor: color,
+      tileColor: itemColor,
       trailing: Row(
         mainAxisSize:
             MainAxisSize.min, // Ensures the Row takes up the minimum space
